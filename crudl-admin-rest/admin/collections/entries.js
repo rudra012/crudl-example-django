c1368741f37f76e3c1d1768ddee8844aa2160db3
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2016-06-06 16:41:02 +0200

    Fieldsets in Entries

--- crudl-admin-rest/admin/collections/entries.js
+++ crudl-admin-rest/admin/collections/entries.js
@@ -71,113 +71,106 @@ var changeView = {
     },
 }
 
-changeView.fields = [
-    {
-        name: 'title',
-        label: 'Title',
-        field: 'Text',
-        required: true,
-    },
-    {
-        name: 'user',
-        label: 'User',
-        field: 'Autocomplete',
-        props: {
-            // allowNone: false,
-            showAll: true,
-        },
-        actions: {
-            select: (req, cxs) => {
-                return Promise.all(req.data.selection.map(item => {
-                    return cxs.user.read(req.with('id', item.value))
-                    .then(res => res.set('data', {
-                        value: res.data.id,
-                        label: res.data.username,
-                    }))
-                }))
-            },
-            search: (req, cxs) => {
-                return cxs.users.read(req.filter('username', req.data.query))
-                .then(res => res.set('data', res.data.map(user => ({
-                    value: user.id,
-                    label: user.username,
-                }))))
-            }
-        }
-    },
-    {
-        name: 'category',
-        label: 'Category',
-        field: 'Autocomplete',
-        watch: [
-            {
-                for: 'user',
-                setProps: user => ({
-                    disabled: !user,
-                    comment: !user ? "In order to select a category, you have to select a user first" : '',
-                }),
-            }
-        ],
-        actions: {
-            select: (req, cxs) => {
-                return Promise.all(req.data.selection.map(item => {
-                    return cxs.category.read(req.with('id', item.value))
-                    .then(res => res.set('data', {
-                        value: res.data.id,
-                        label: res.data.name,
-                    }))
-                }))
-            },
-            search: (req, cxs) => {
-                if (!req.context.user) {
-                    return Promise.resolve({data: []})
-                } else {
-                    return cxs.categories.read(req
-                        .filter('name', req.data.query)
-                        .filter('user', req.context.user))
-                    .then(res => res.set('data', res.data.map(d => ({
-                        value: d.id,
-                        label: `<b>${d.name}</b> (${d.slug})`,
-                    }))))
-                }
-            },
-        },
-    },
-    {
-        name: 'date',
-        label: 'Date',
-        field: 'Date',
-    },
-    {
-        name: 'body',
-        label: 'Text',
-        field: 'Text',
-    },
-    {
-        name: 'tags',
-        label: 'Tags',
-        field: 'Autocomplete',
-        props: {
-            multiple: true,
-        },
-        actions: {
-            search: (req, cxs) => {
-                return cxs.tags_options.read(req)
-                .then(res => res.set('data', res.data.filter(tag => {
-                    return tag.label.toLowerCase().indexOf(req.data.query.toLowerCase()) >= 0
-                })))
-            },
-            select: (req, cxs) => {
-                return Promise.all(req.data.selection.map(item => {
-                    return cxs.tag.read(req.with('id', item.value))
-                    .then(res => res.set('data', {
-                        value: res.data.id,
-                        label: res.data.name,
-                    }))
-                }))
-            },
-        },
-    },
+changeView.fieldsets = [
+   {
+       fields: [
+           {
+               name: 'title',
+               label: 'Title',
+               field: 'Text',
+               required: true,
+           },
+           {
+               name: 'user',
+               label: 'User',
+               field: 'Select',
+               props: {
+                   canBeNone: true,
+                   helpText: 'Select a user'
+               },
+               actions: {
+                   asyncProps: (req, cxs) => cxs.users_options.read(req),
+               },
+           },
+       ],
+   },
+   {
+       title: 'Category',
+       expanded: true,
+       fields: [
+           {
+               name: 'category',
+               label: 'Category',
+               field: 'Autocomplete',
+               props: {
+                   helpText: "Select a category",
+               },
+               watch: [
+                   {
+                       for: 'user',
+                       setProps: user => ({
+                           disabled: !user,
+                           comment: !user ? "In order to select a category, you have to select a user first" : '',
+                       }),
+                   }
+               ],
+               actions: {
+                   select: (req, cxs) => {
+                       return Promise.all(req.data.selection.map(item => {
+                           return cxs.category.read(req.with('id', item.value))
+                           .then(res => res.set('data', {
+                               value: res.data.id,
+                               label: res.data.name,
+                           }))
+                       }))
+                   },
+                   search: (req, cxs) => {
+                       if (!req.context.user) {
+                           return Promise.resolve({data: []})
+                       } else {
+                           return cxs.categories.read(req
+                               .filter('name', req.data.query)
+                               .filter('user', req.context.user))
+                           .then(res => res.set('data', res.data.map(d => ({
+                               value: d.id,
+                               label: `<b>${d.name}</b> (${d.slug})`,
+                           }))))
+                       }
+                   },
+               },
+           },
+       ],
+   },
+   {
+       title: 'Bla',
+       expanded: true,
+       fields: [
+           {
+               name: 'date',
+               label: 'Date',
+               field: 'Datetime',
+           },
+           {
+               name: 'body',
+               label: 'Text',
+               field: 'Text',
+           },
+           {
+               name: 'tags',
+               label: 'Tags',
+               field: 'SelectMultiple',
+               props: {
+                   multiple: true,
+               },
+               actions: {
+                   asyncProps: (req, cxs) => cxs.tags_options.read(req)
+                   .then(res => res.set('data', {
+                       options: res.data,
+                   })),
+               },
+           },
+       ]
+   }
 ]
 
 changeView.tabs = [
@@ -212,7 +205,7 @@ changeView.tabs = [
 var addView = {
     path: 'entries/new',
     title: 'New Blog Entry',
-    fields: changeView.fields,
+    fieldsets: changeView.fieldsets,
     actions: {
         add: function (req, cxs) { return cxs.entries.create(req) },
     },
