e8aebfb9b49039fd92a8cb0859938e31aea3fc03
Vaclav Mikolasek <vaclav.mikolasek@gmail.com>
2016-07-03 23:02:54 +0200

    More robust join function

--- crudl-admin-rest/admin/utils.js
+++ crudl-admin-rest/admin/utils.js
@@ -1,9 +1,12 @@
 
-function join(p1, p2, var1, var2) {
+function join(p1, p2, var1, var2, defaultValue={}) {
     return Promise.all([p1, p2])
     .then(responses => {
         return responses[0].set('data', responses[0].data.map(item => {
             item[var1] = responses[1].data.find(obj => obj[var2] == item[var1])
+            if (!item[var1]) {
+                item[var1] = defaultValue
+            }
             return item
         }))
     })
